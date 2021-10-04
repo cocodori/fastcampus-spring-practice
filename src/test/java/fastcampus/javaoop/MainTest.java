413f5ce7b70a2b33b67cac2bd9db2b267254a82d
hoon <moradulee@gmail.com>
2021-10-04 16:14:40 +0900

    Initialize #1 Java project
    지난 시간 만들었던 프로젝트 복사해서 가져오기

---
+++ src/test/java/fastcampus/javaoop/MainTest.java
@@ -0,0 +1,14 @@
+package fastcampus.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Main.main(new String[]{"3", "1", "2"});
+    }
+
+}
---
+++ src/test/java/fastcampus/javaoop/logic/JavaSortTest.java
@@ -0,0 +1,24 @@
+package fastcampus.javaoop.logic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaSortTest {
+
+    @Test
+    @DisplayName("Java 소트 - 리스트를 넣으면 정렬된 결과를 보여준다")
+    void given_List_WhenExecuting_ThenReturnSortedList() {
+        // given
+        JavaSort<Integer> javaSort = new JavaSort<>();
+
+        // when
+        List<Integer> actual = javaSort.sort(List.of(3, 4, 1, 2, 5));
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5), actual);
+    }
+}
