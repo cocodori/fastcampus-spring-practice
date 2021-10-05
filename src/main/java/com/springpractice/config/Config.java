8670a1aa4ec5ede840dca4429399bb1258937a28
hoon <hoon.lee@jalsalda.com>
2021-10-05 19:49:56 +0900

    Config #1 - spring configuration

---
+++ src/main/java/com/springpractice/config/Config.java
@@ -0,0 +1,10 @@
+package com.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.springpractice")
+@Configuration
+public class Config {
+
+}
