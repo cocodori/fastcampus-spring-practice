a025d13ce0c08eb82685b6433a22f4238decb02c
hoon <moradulee@gmail.com>
2021-10-06 19:59:05 +0900

    #1 SpringWebMvc
    Spring WebMVC 설정하고 톰캣 띄우기

--- src/main/java/com/springpractice/config/Config.java
+++ src/main/java/com/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package com.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.springpractice")
 @Configuration
 public class Config {
